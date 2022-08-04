6d6ca8bbb0b7e020c071146723ad1eb596548a56
Aybek Amanbaev <idevkun@gmail.com>
2022-08-04 23:43:39 +0500

    6. Why React? Its Composable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+function Navbar() {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Portfolio</li>
+      </ul>
+    </nav>
+  )
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1 className="title">Bul jerde content</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="container">
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
+      <Navbar />
+      <MainContent/>
     </div>
   );
 }
