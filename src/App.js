18a403c16e8729154bf21378dfc0505bb4274c14
Aybek Amanbaev <idevkun@gmail.com>
2022-08-05 00:01:55 +0500

    9. JSX

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 function App() {
- /*  <script>
-    function addArtistNameToBody() {
-      const bodyTag = document.querySelector("#root");
-    const divTag = document.createElement("div");
-    let h1Tag = document.createElement("h1");
-    h1Tag.innerText = "Mitski";
-    divTag.append(h1Tag);
-    bodyTag.append(divTag);
-  }
-
-    addArtistNameToBody();
-  </script> */
   return (
     <div className="container">
-      <h1>Mitski</h1>
+      {console.log(<h1>Mitski</h1>)}
     </div>
   );
 }
