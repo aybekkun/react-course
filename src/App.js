9555d88271f95e1b8daa21baef6a7bebec5e9dc9
Aybek Amanbaev <idevkun@gmail.com>
2022-08-04 23:20:10 +0500

    4. First React Practice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="container">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
     </div>
   );
 }
